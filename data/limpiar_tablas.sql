e7dfb0496c3155256f61f19e1b8cc0a3c5d203e5
damianvc31 <damiancohen93@gmail.com>
2017-05-12 21:41:56 -0300

    Agrego scripts limpiar y borrar tablas

---
+++ data/limpiar_tablas.sql
@@ -0,0 +1,28 @@
+USE [GD1C2017]
+GO
+
+DELETE FROM LJDG.[Rol_Usuario
+GO
+
+DELETE FROM LJDG.Funcionalidad_x_Rol
+GO
+
+DELETE FROM LJDG.Funcionalidad
+GO
+
+DELETE FROM LJDG.Rol
+GO
+
+DELETE FROM LJDG.Automovil
+GO
+
+DELETE FROM LJDG.Marca
+GO
+
+DELETE FROM LJDG.Usuario
+GO
+
+DELETE FROM LJDG.Turno
+GO
+
+
