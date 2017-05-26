c7d6093f034cb20c44eb8edd82b920ae04c11789
JPfx <jpbulbulian@gmail.com>
2017-05-26 01:32:02 -0300

    SP

--- data/scripts stored procedures/busqueda/buscar_auto.sql
+++ data/scripts stored procedures/busqueda/buscar_auto.sql
@@ -11,13 +11,19 @@ CREATE PROCEDURE LJDG.buscar_auto
 	@chofer INT
 AS
 BEGIN
-	SELECT auto_id 'ID', marc_nombre 'Marca', auto_modelo 'Modelo', auto_patente 'Patente', auto_chofer 'Chofer ID', chof_apellido 'Apellido'
+	SELECT auto_id 'ID',
+		marc_nombre 'Marca',
+		auto_modelo 'Modelo',
+		auto_patente 'Patente',
+		auto_chofer 'Chofer ID',
+		chof_nombre 'Nombre',
+		chof_apellido 'Apellido'
 	FROM LJDG.Automovil
 		JOIN LJDG.Marca ON auto_marca = marc_id
 		JOIN LJDG.Chofer ON auto_chofer = chof_id
 	WHERE auto_modelo LIKE @modelo + '%'
 		AND auto_patente LIKE @patente + '%'
-		OR ( @marca = 0 OR auto_marca = @marca)
+		AND ( @marca = 0 OR auto_marca = @marca)
 		OR ( @chofer = 0 OR auto_chofer = @chofer)
 END
 GO
--- data/scripts stored procedures/cliente/baja_cliente.sql
+++ data/scripts stored procedures/cliente/baja_cliente.sql
@@ -1,32 +1,15 @@
 USE GD1C2017
 
-IF EXISTS (SELECT name FROM sysobjects WHERE name='alta_auto' AND type='p')
-DROP PROCEDURE LJDG.alta_auto
+IF EXISTS (SELECT name FROM sysobjects WHERE name='baja_cliente' AND type='p')
+DROP PROCEDURE LJDG.baja_cliente
 GO
 
-CREATE PROCEDURE LJDG.alta_auto
-	@marca INT,
-	@patente VARCHAR(10),
-	@modelo VARCHAR(255),
-	@chofer INT,
-	@turno INT,
-	@habilitado BIT,
-	@mensaje VARCHAR(200) OUT
+CREATE PROCEDURE LJDG.baja_cliente
+	@id INT
 AS
 BEGIN
-	IF @patente = ''
-		SET @mensaje = 'Ingrese la Patente'
-	ELSE IF @patente IN ( SELECT auto_patente FROM LJDG.Automovil )
-		SET @mensaje = 'La Patente Ingresada ya existe'
-	ELSE IF @modelo = ''
-		SET @mensaje = 'Ingrese el Modelo'
-	ELSE
-	BEGIN
-		INSERT INTO LJDG.Automovil
-		( auto_marca, auto_patente, auto_modelo, auto_chofer, auto_turno, auto_habilitado )
-		VALUES
-		( @marca, @patente, @modelo, @chofer, @turno, @habilitado )
-		SET @mensaje = 'Carga Exitosa'
-	END
+	UPDATE LJDG.Cliente
+	SET clie_habilitado = 0
+	WHERE clie_id = @id
 END
 GO
