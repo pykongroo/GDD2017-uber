e8e2fdac4ce382184cf37cb9d9540f9ba8dfcf2b
damianvc31 <damiancohen93@gmail.com>
2017-05-30 03:04:05 -0300

    Solucion migraciones factura y rendicion que tardaban mucho, sin sumar duplicados
    Primero se hace el INSERT de las facturas/rendiciones sin importe total
    (se deja en NULL), desde la tabla maestra, luego se hace UPDATE
    utilizando la tabla viaje para hacer el SUM.

--- data/scripts migracion/migracion_facturas.sql
+++ data/scripts migracion/migracion_facturas.sql
@@ -12,14 +12,9 @@ INSERT INTO [LJDG].[Factura]
            ,[fact_fecha_inicio]
            ,[fact_fecha_fin]
            ,[fact_fecha]
-           ,[fact_cliente]
-           ,[fact_importe_total])
+           ,[fact_cliente])
 SELECT   m.Factura_Nro,m.Factura_Fecha_Inicio,m.Factura_Fecha_Fin,m.Factura_Fecha,
-		 (select clie_id from LJDG.Cliente where clie_dni = m.Cliente_Dni),
-		 sum(Viaje_Cant_Kilometros * Turno_Valor_Kilometro + Turno_Precio_Base) -- incluye viajes duplicados que se facturan dos veces
-		 /*(select sum(viaj_precio) from LJDG.Viaje  --evita sumar duplicados pero tarda 1 minuto en terminar
-			 where LJDG.viaje_entra_en_factura(viaj_fecha_inicio,m.Factura_Fecha_Inicio,m.Factura_Fecha_Fin) = 1 
-							and viaj_cliente = (select clie_id from LJDG.Cliente where m.Cliente_Dni = clie_dni))*/
+		 (select clie_id from LJDG.Cliente where clie_dni = m.Cliente_Dni)
 FROM gd_esquema.Maestra m
 WHERE Factura_Nro is not null
 GROUP BY m.Factura_Nro,m.Factura_Fecha_Inicio,m.Factura_Fecha_Fin,m.Factura_Fecha,m.Cliente_Dni
@@ -29,3 +24,8 @@ GO
 SET IDENTITY_INSERT LJDG.Factura OFF
 GO
 
+UPDATE LJDG.Factura
+SET fact_importe_total = (select sum(viaj_precio) from LJDG.Viaje
+						where LJDG.viaje_entra_en_factura(viaj_fecha_inicio,fact_fecha_inicio,fact_fecha_fin) = 1 AND
+								viaj_cliente = fact_cliente)
+GO
--- data/scripts migracion/migracion_rendiciones.sql
+++ data/scripts migracion/migracion_rendiciones.sql
@@ -11,17 +11,10 @@ INSERT INTO [LJDG].[Rendicion]
            ([rend_nro]
            ,[rend_fecha]
            ,[rend_turno]
-           ,[rend_chofer]
-           ,[rend_importe_total])
+           ,[rend_chofer])
 SELECT  m.Rendicion_Nro,m.Rendicion_Fecha,
 		(select turn_id from LJDG.Turno where turn_descripcion = m.Turno_Descripcion),
-		 (select chof_id from LJDG.Chofer where chof_dni = m.Chofer_Dni),
-		 sum(m.Rendicion_Importe) --suma duplicados
-		 /* --evita sumar dos veces el importe de los viajes con rendicion duplicada pero tarda 5 minutos
-		 (select sum(viaj_importe_rend) from LJDG.Viaje  
-			 where LJDG.viaje_entra_en_rendicion(viaj_fecha_inicio,m.Rendicion_Fecha) = 1  --mismo dia ignorando hora
-							and viaj_chofer = (select chof_id from LJDG.Chofer where m.Chofer_Dni = chof_dni)
-							and viaj_turno = (select turn_id from LJDG.Turno where m.Turno_Descripcion = turn_descripcion))*/
+		 (select chof_id from LJDG.Chofer where chof_dni = m.Chofer_Dni)
 FROM gd_esquema.Maestra m
 WHERE Rendicion_Nro is not null
 GROUP BY m.Rendicion_Nro,m.Rendicion_Fecha,m.Turno_Descripcion,m.Chofer_Dni
@@ -31,5 +24,10 @@ GO
 SET IDENTITY_INSERT LJDG.Rendicion OFF
 GO
 
+UPDATE LJDG.Rendicion
+SET rend_importe_total = (select sum(viaj_importe_rend) from LJDG.Viaje
+						where LJDG.viaje_entra_en_rendicion(viaj_fecha_inicio,rend_fecha) = 1 AND
+								viaj_chofer = rend_chofer AND viaj_turno = rend_turno)
+GO
 
 
